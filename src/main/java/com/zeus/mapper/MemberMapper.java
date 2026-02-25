4e9081e6470615393ca76256b02b1a20f2bbadfd
hy3oning <shyun5635@gmail.com>
2026-02-25 11:48:18 +0900

    [update] 줄이동

--- src/main/java/com/zeus/mapper/MemberMapper.java
+++ src/main/java/com/zeus/mapper/MemberMapper.java
@@ -24,14 +24,16 @@ public interface MemberMapper {
 	// 수정 처리
 	int modify(Member member) throws Exception;
 
+	// 삭제 처리
+	int remove(int userNo) throws Exception;
+
 	// 권한 추가 처리
 	int modifyAuth(MemberAuth memberAuth) throws Exception;
 
 	// 권한 삭체 처리
 	int deleteAuth(@Param("userNo") int userNo) throws Exception;
 
-	int remove(int userNo) throws Exception;
-
+	// 전체카운트
 	int countAll() throws Exception;
 
 }
