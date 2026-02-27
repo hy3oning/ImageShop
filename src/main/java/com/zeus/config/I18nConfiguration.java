5f0309593cbb64c41f9915e66b9839f84053016e
hy3oning <shyun5635@gmail.com>
2026-02-27 23:31:19 +0900

    [update] 메시지프로퍼티가 말썽입니다..

--- src/main/java/com/zeus/config/I18nConfiguration.java
+++ src/main/java/com/zeus/config/I18nConfiguration.java
@@ -3,7 +3,7 @@ package com.zeus.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -12,16 +12,11 @@ public class I18nConfiguration {
 
 	@Bean
 	MessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("message.message");
-
-		// ✅ UTF-8 강제 지정 (핵심)
-		messageSource.setDefaultEncoding("UTF-8");
-
-		// (선택) 메시지 못 찾을 때 키 그대로 출력
-		messageSource.setUseCodeAsDefaultMessage(true);
-
-		return messageSource;
+		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+		ms.setBasename("classpath:message/message");
+		ms.setDefaultEncoding("UTF-8");
+		ms.setUseCodeAsDefaultMessage(true);
+		return ms;
 	}
 
 	@Bean
